fa074d360c7636126922ce426178ef270980d746
txtFn <ennius33@gmail.com>
2023-10-03 14:23:25 +0800

    Initial commit

---
+++ javascript/index.js
@@ -0,0 +1,32 @@
+// ScrollSpy
+$(document).ready(function () {
+    $('.scrollspy').scrollSpy();
+});
+
+// Carousel
+$(document).ready(function () {
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+});
+
+// Sidenav
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+// Parallax
+$(document).ready(function () {
+    $('.parallax').parallax();
+});
+
+// Dropdown
+$(document).ready(function () {
+    $('.dropdown-trigger').dropdown();
+});
+
+// Modal
+$(document).ready(function () {
+    $('.modal').modal();
+});
